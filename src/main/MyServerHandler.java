dd27ccf7cd12be58f27b22d2366927709c06144f
northdigital <giotis@email.com>
2018-04-17 23:14:59 +0300

    commit

--- src/main/MyServerHandler.java
+++ src/main/MyServerHandler.java
@@ -1,7 +1,6 @@
 package main;
 
 import io.netty.buffer.ByteBuf;
-import io.netty.buffer.Unpooled;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
 import io.netty.util.CharsetUtil;
@@ -17,12 +16,6 @@ public class MyServerHandler extends ChannelInboundHandlerAdapter {
   public void channelRead(ChannelHandlerContext ctx, Object msg) {
     ByteBuf in = (ByteBuf) msg;
     mainController.log("Received->" + in.toString(CharsetUtil.UTF_8));
-    ctx.write(in);
-  }
-
-  @Override
-  public void channelReadComplete(ChannelHandlerContext ctx) {
-    ctx.writeAndFlush(Unpooled.EMPTY_BUFFER);
   }
 
   @Override
